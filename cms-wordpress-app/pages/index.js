500ee68bef92406c8fed66dc8f9807233ff0e280
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-23 22:48:44 -0500

    updates

--- cms-wordpress-app/pages/index.js
+++ cms-wordpress-app/pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ allPosts: { edges }, preview }) {
 		<>
 			<Layout preview={preview}>
 				<Head>
-					<title>Next.js Blog Example with {CMS_NAME}</title>
+					<title>Next.js and {CMS_NAME}</title>
 				</Head>
 				<Container>
 					<Intro />
