ea279c17d703a932333284886bd651502741319a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-23 22:54:18 -0500

    updates

--- cms-wordpress-app/lib/constants.js
+++ cms-wordpress-app/lib/constants.js
@@ -1,4 +1,4 @@
-export const EXAMPLE_PATH = "cms-wordpress";
+export const EXAMPLE_PATH = "nextjs-wordpress";
 export const CMS_NAME = "WordPress";
 export const CMS_URL = "https://wordpress.org";
 export const HOME_OG_IMAGE_URL =
