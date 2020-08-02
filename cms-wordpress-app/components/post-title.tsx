14ba4847fbd1fcfbb384c709edf7f0a675b25a95
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-02 14:29:35 -0500

    post-title tsx configured

---
+++ cms-wordpress-app/components/post-title.tsx
@@ -0,0 +1,12 @@
+type PostTitleProps = {
+	children: string;
+};
+
+export default function PostTitle({ children }: PostTitleProps) {
+	return (
+		<h1
+			className='text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left'
+			dangerouslySetInnerHTML={{ __html: children }}
+		/>
+	);
+}
